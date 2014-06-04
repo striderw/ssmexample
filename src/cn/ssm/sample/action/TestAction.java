185e1be62bb86bc76076543a45377bd443918325
striderw <taotie119@gmail.com>
2014-06-04 14:56:49 +0800

    test syop

--- src/cn/ssm/sample/action/TestAction.java
+++ src/cn/ssm/sample/action/TestAction.java
@@ -1,18 +1,13 @@
 package cn.ssm.sample.action;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
 import cn.ssm.sample.dto.User;
 import cn.ssm.sample.service.IndexSvr;
-
-import com.opensymphony.xwork2.ActionSupport;
 
 @Controller
 public class TestAction extends ActionSupport{
 
 	private User user;
-	
+
 	public User getUser() {
 		return user;
 	}
@@ -27,6 +22,7 @@ public class TestAction extends ActionSupport{
 	@Override
 	public String execute() throws Exception {
 		user = indexSvr.getUser(1);
+		System.out.println("test");
 		return super.execute();
 	}
 
